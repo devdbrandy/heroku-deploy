e4883f46c7aaa6de9b08b4d1108cdce41c11f156
Harry Dbrandy <devdbrandy@gmail.com>
2017-10-17 15:09:15 +0100

    Implement server rendering

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.send('Hello Express'));
+
+app.listen(3000, () => console.log('Live on port 3000'));
